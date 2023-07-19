f106eb11f13384e83b7afc0361d949c34c427892
embed-Rayn <57181674+embed-Rayn@users.noreply.github.com>
2023-07-19 16:49:52 +0900

    player 추가하여 교차 선택기능 추가

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 
 function Square(props){
     return(
-        <button className='square' onClick={props.onClick()}>
+        <button className='square' onClick={props.onClick}>
             {props.value}
         </button>
-    )
+    );
 }
 
 class Board extends React.Component {
@@ -15,12 +15,16 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares, 
+            xIsNext: !this.state.xIsNext
+        });
     }
     renderSquare(i) {
         return (
@@ -32,7 +36,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = "Next player: X";
+        const status = "Next player: " + (this.state.xIsNext? 'X': 'O');
 
         return (
         <div>
